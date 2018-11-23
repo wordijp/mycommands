d4bb74974d2d091f757bebb7b3bcfdc83a426ac9
wordi <wordijp@gmail.com>
2018-11-23 20:07:05 +0900

    php -lを並列化

--- internal/php-lint-run.rb
+++ internal/php-lint-run.rb
@@ -1,3 +1,4 @@
+require 'parallel'
 require 'open3'
 
 # original)
@@ -5,12 +6,51 @@ require 'open3'
 #  xargs -I'{}' -n1 php -l {} 2>&1 1>/dev/null | \
 #  grep --line-buffered -v -E '^No syntax errors'
 
-Open3.popen3('fd -HI --type f -E .git -E vendor ".*\.php$" | sed -E "s/\\\\\\\\/\\\\//g"') {|i, o|
-  o.each do |line|
-    Open3.popen3("php -l #{line}") {|_, _, e2|
-      e2.each do |line2|
-        print line2 unless line2.match(/^No syntax errors/)
+
+def main
+  cmd = 'fd -HI --type f -E .git -E vendor ".*\.php$" | sed -E "s/\\\\\\\\/\\\\//g"'
+  files = `#{cmd}`.lines.map {|file| file.chomp}
+
+  threads = Parallel.processor_count
+  Parallel.each_with_index(files, in_threads: threads) do |file, i|
+    Open3.popen3("php -l #{file}") do |_, o, e|
+      # check have error
+      error = true
+      o.each do |line|
+        if line.index('No syntax errors') != nil
+          error = false
+          break
+        end
       end
-    }
+
+      msg = e.first
+      stablePrint(i, error, msg)
+    end
   end
-}
+end
+
+
+$msgs = {}
+$cur_i = 0
+
+# 順番を保ったまま出力
+def stablePrint(i, error, msg)
+  # メッセージ更新
+  $msgs[i] = {
+    error: error,
+    msg: msg
+  }
+
+  # メッセージ表示
+  while $msgs.has_key?($cur_i)
+    msg_ = $msgs[$cur_i]
+    if msg_[:error]
+      print msg_[:msg]
+    end
+
+    $msgs.delete($cur_i)
+    $cur_i += 1
+  end
+end
+
+main
