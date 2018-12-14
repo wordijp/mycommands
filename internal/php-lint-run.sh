b0af4f2eb89d3971d5abe8f002864a0aa2b342da
wordi <wordijp@gmail.com>
2018-12-15 04:04:36 +0900

    並列数をCPUコア数へ

--- internal/php-lint-run.sh
+++ internal/php-lint-run.sh
@@ -1,3 +1,5 @@
+ncpu=`cat /proc/cpuinfo | grep processor | wc -l | tr -d '\n'`
+
 fd -HI --type f -E .git -E vendor '.*\.php' | sed -u -E 's/\\/\//g' | \
-  xargs -n1 -P4 php -l 2>&1 1>/dev/null | \
+  xargs -n1 -P$ncpu php -l 2>&1 1>/dev/null | \
   grep --line-buffered -E .
