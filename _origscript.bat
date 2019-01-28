f0d75a9eebaf769d857d7e50b882e175571bf414
wordi <wordijp@gmail.com>
2019-01-28 21:16:40 +0900

    rubyに--disable-gemsを付与
    gems依存のコード書かないし、起動時間が短縮される

--- _origscript.bat
+++ _origscript.bat
@@ -5,7 +5,7 @@
 ) else if exist %~dpn0.py (
   @python -u %~dpn0.py %*
 ) else if exist %~dpn0.rb (
-  @ruby -r%~dp0_preproc.rb %~dpn0.rb %*
+  @ruby -r%~dp0_preproc.rb --disable=gems %~dpn0.rb %*
 ) else (
   @echo script not found: %~n0
 )
